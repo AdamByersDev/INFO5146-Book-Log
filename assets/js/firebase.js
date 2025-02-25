ec785c38267e5e45d3496ec96dc7ae8abdb77eee
Adam Byers <adam@abyers.ca>
2025-02-25 03:40:54 -0500

    Told eslint ITS FINE

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAsINr4MAe6ajjpBLJhK-7DI_mt9mhejTs",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const db = getFirestore(app);
